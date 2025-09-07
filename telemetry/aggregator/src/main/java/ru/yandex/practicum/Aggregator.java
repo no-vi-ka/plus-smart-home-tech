2500a1d381572658c0ce7e4531f84d98982df0ff
Viktoriia Novikova <koncord@yandex.ru>
2025-09-07 14:56:06 +0300

    Добавить класс Aggregator в git.

---
+++ telemetry/aggregator/src/main/java/ru/yandex/practicum/Aggregator.java
@@ -0,0 +1,15 @@
+package ru.yandex.practicum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import ru.yandex.practicum.starter.AggregationStarter;
+
+@SpringBootApplication
+public class Aggregator {
+    public static void main(String[] args) {
+        ConfigurableApplicationContext context = SpringApplication.run(Aggregator.class, args);
+        AggregationStarter aggregator = context.getBean(AggregationStarter.class);
+        aggregator.start();
+    }
+}
